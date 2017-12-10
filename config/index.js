fa998507886e1bab838c92b03246ee14945c29a4
Basic Design <amos_mac@Amos-mac.local>
2017-12-11 00:05:21 +0800

    port 8080

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: process.env.PORT || 80,
+    port: process.env.PORT || 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
